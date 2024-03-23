7be6d6c0a8bfb8eb89a005d286944a6237673981
Mbaka11 <90229093+Mbaka11@users.noreply.github.com>
2024-03-23 00:42:08 -0400

    add script

---
+++ myscript.py
@@ -0,0 +1,8 @@
+import os
+
+good_commit = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+first_bad_commit = "c1a4be04b972b6c17db242fc37752ad517c29402"
+
+os.system(f"git bisect start {first_bad_commit} {good_commit}")
+os.system("git bisect run python manage.py test")
+os.system("git bisect reset")
